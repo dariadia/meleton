fcf21f504d64db90c0c309af5ba98233ad56411d
Daria <daria.diachkova@mail.ru>
2024-03-05 11:27:29 +0300

    feat(components): add sample Alert

---
+++ components/Alert.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-alert dense dismissible elevation="2" icon="mdi-account" outlined prominent shaped text color="primary">
+    You have an event soon!
+  </v-alert>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="10">
       <div class="text-h3 mb-4 text-center">Meleton: My Calendar</div>
+      <Alert />
       <Calendar />
     </v-col>
   </v-row>
