510200de16010e6647ac4906eeec2d24d1d80317
Daria <daria.diachkova@mail.ru>
2024-03-05 10:30:14 +0300

    feat(app): add sample Calendar

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        hello world
-      </v-card>
+      hello world
+      <Calendar />
     </v-col>
   </v-row>
 </template>
