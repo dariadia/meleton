af4a8c2a76720394b0838a0674b48ca91050fba5
Daria <daria.diachkova@mail.ru>
2024-03-06 07:43:45 +0300

    feat(app): use other storage key

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
 export default {
   name: 'IndexPage',
   data: () => ({
-    localStorageKey: "calendarEvents",
+    localStorageKey: "calendar_events",
     hasDue: [],
     interval: null,
     MINUTE: 60 * 1000,
