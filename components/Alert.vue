18dcabe099e5dac47708ae14266127fa20fd05a0
Daria <daria.diachkova@mail.ru>
2024-03-06 07:42:35 +0300

    feat(components): split logic

--- components/Alert.vue
+++ components/Alert.vue
@@ -7,6 +7,28 @@
 
 <script>
 export default {
-  props: ['event', 'dueTime'],
+  props: ['event'],
+  data: () => ({
+    dueTime: null,
+    checkInterval: null,
+    MINUTE: 60 * 1000,
+  }),
+  mounted() {
+    this.calcDue()
+    this.checkInterval = setInterval(this.calcDue, this.MINUTE)
+  },
+  onBeforeUnmount() {
+    clearInterval(this.checkInterval)
+  },
+  methods: {
+    calcDue() {
+      let nowDate = new Date()
+      let achiveDate = new Date(this.event.start)
+      var result = (achiveDate - nowDate) + 1000
+      var minutes = Math.floor((result / 1000 / 60) % 60)
+      if (minutes < 10) minutes = '0' + minutes
+      this.dueTime = minutes
+    }
+  }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" sm="8" md="10">
       <v-container class="text-h3 mb-4 text-center">Meleton: My Calendar</v-container>
       <v-container>
-        <Alert v-for="item in hasDue" :key="item.id" :event="item" :dueTime="dueTime" />
+        <Alert v-for="item in hasDue" :key="item.id" :event="item" />
       </v-container>
       <Calendar :checkIfHasDue="checkIfHasDue" :localStorageKey="localStorageKey" />
     </v-col>
@@ -17,12 +17,11 @@ export default {
     localStorageKey: "calendarEvents",
     hasDue: [],
     interval: null,
-    dueTime: null,
+    MINUTE: 60 * 1000,
   }),
   mounted() {
-    const MINUTE = 60 * 1000
     this.checkIfHasDue()
-    this.interval = setInterval(() => this.checkIfHasDue(), MINUTE)
+    this.interval = setInterval(this.checkIfHasDue, this.MINUTE)
   },
   onBeforeUnmount() {
     clearInterval(this.interval)
@@ -33,26 +32,12 @@ export default {
       if (!_data) return
       const _events = JSON.parse(_data)
       const checkTime = (date) => {
-        const HOUR = 1000 * 60 * 60
+        const HOUR = this.MINUTE * 60
         const _date = new Date(date)
         const isFuture = Date.now() - _date <= 0
         return isFuture ? _date - HOUR <= Date.now() : _date + HOUR <= Date.now()
       }
       this.hasDue = _events.filter(event => checkTime(event.start))
-      
-      if (this.hasDue.length) {
-        /* simplification: We could run this inside <Alert /> 
-        but I would rather not have two separate intervals running */
-        this.hasDue.forEach(event => this.calcDue(event))
-      }
-    },
-    calcDue(event) {
-      let nowDate = new Date()
-      let achiveDate = new Date(event.start)
-      var result = (achiveDate - nowDate) + 1000
-      var minutes = Math.floor((result / 1000 / 60) % 60)
-      if (minutes < 10) minutes = '0' + minutes
-      this.dueTime = minutes
     }
   }
 }
