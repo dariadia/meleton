9c522fb1e604dc3ee621576771ce493fd7088f52
Daria <daria.diachkova@mail.ru>
2024-03-06 08:52:26 +0300

    chore(components): split

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -17,16 +17,7 @@
           </v-btn>
           <v-toolbar-title v-if="$refs.calendar">{{ $refs.calendar.title }}</v-toolbar-title>
           <v-row class="ml-4">
-            <v-dialog v-model="isOpenYearPicker" width="500">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn color="primary" v-bind="attrs" v-on="on">
-                  Set M/Y
-                </v-btn>
-              </template>
-              <v-card>
-                <v-date-picker full-width v-model="dateInView" @change="goToDate" class="mt-4"></v-date-picker>
-              </v-card>
-            </v-dialog>
+            <YearPicker :isOpen="isOpenYearPicker" :setFocus="setFocus" />
           </v-row>
           <v-menu bottom right>
             <template v-slot:activator="{ on }">
@@ -127,7 +118,6 @@
 export default {
   props: ['checkIfHasDue', 'localStorageKey'],
   data: () => ({
-    dateInView: new Date().toISOString().substr(0, 10),
     today: new Date().toISOString().substr(0, 10),
     focus: new Date().toISOString().substr(0, 10),
     type: 'month',
@@ -177,16 +167,16 @@ export default {
       if (!_data) return
       this.events = JSON.parse(_data)
     },
-    goToDate() {
-      this.focus = this.dateInView
-    },
     setPopupDate({ date, time }) {
       this.popupDate = true
-      this.focus = date
+      this.setFocus(date)
       this.start = `${date} ${time || "00:00"}`
     },
-    viewDay({ date }) {
+    setFocus(date) {
       this.focus = date
+    },
+    viewDay({ date }) {
+      this.setFocus(date)
       this.type = 'day'
     },
     getEventColor(event) {
@@ -201,7 +191,7 @@ export default {
         this.popup = false
     },
     setToday() {
-      this.focus = this.today
+      this.setFocus(this.today)
     },
     prev() {
       this.$refs.calendar.prev()
